b7ab990aab8f12c22b103bd57ccd0f1260aa9f2e
Juliet <agutu@gmail.com>
2022-01-06 11:40:04 +0300

    add jquery function

--- js/script.js
+++ js/script.js
@@ -3,3 +3,25 @@
 // $(document).ready(function() {
 //     alert($);
 // });
+
+// $(function() {
+//     $("#img1").hover(function() //mouseOver
+    
+//     {
+//        $("#img1").attr("src")
+//     },
+
+//         function() //mouseOut
+
+//     {})
+// });
+
+// $(document).ready( function() {
+
+//     $("#img1").hover( function() {
+//         $(this).find(".images/work1.jpg").fadeIn(300);
+//     }, function() {
+//         $(this).find(".images/work2.jpg").fadeOut(100);
+//     });
+    
+// });
